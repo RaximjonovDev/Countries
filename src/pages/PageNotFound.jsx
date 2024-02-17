78b13e1e1eea98b2d27ba2fbe565705e115ffbab
RaximjonovDev <abdulazizraximjonov7@gmail.com>
2024-02-17 20:56:03 +0500

    PageNotFound

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PageNotFound = () => {
+  return (
+    <div className='absolute top-[320px] left-[475px]'>
+        <h1 className='text-5xl font-semibold'>PAGE NOT FOUND</h1>
+    </div>
+  )
+}
+
+export default PageNotFound
