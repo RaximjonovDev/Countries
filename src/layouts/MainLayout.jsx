a4be69dbc7b92b6450a0d4b73686375f7eb2580b
RaximjonovDev <abdulazizraximjonov7@gmail.com>
2024-02-17 21:08:15 +0500

    Main-Layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react'
 import Header from '../components/Header'
 import { Outlet, useLocation } from 'react-router-dom'
-import BreadCrump from '../components/BreadCrump'
+import BreadCrumb from '../components/BreadCrumb'
 
 const MainLayout = () => {
   const location = useLocation()
@@ -14,7 +14,7 @@ const MainLayout = () => {
         {!HomePage && 
         <div className='dark:bg-midDark dark:text-white'> 
           <div className='w-full max-w-7xl mx-auto px-5 pt-5'>
-          <BreadCrump/>
+          <BreadCrumb/>
         </div>
         </div>
         }
