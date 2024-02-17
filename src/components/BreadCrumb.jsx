028fa47406e2c35b33b2b205dc8146d9e45b01c1
RaximjonovDev <abdulazizraximjonov7@gmail.com>
2024-02-17 20:54:16 +0500

    Bread-Crumb

---
+++ src/components/BreadCrumb.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
+
+const BreadCrump = () => {
+
+    const location = useLocation();
+    const pathArr = location.pathname.split('/').filter(path => path.trim() !== ''); 
+    const cleanedPathArr = pathArr.map(path => path.replace(/20|%20/g, ' ')); 
+    
+
+
+    return (
+        <ul className='flex space-x-2 '>
+            <li>
+                <Link>Home</Link>
+            </li>
+            {
+                cleanedPathArr.map((path, index) => {
+                    return (
+                        <li key={index}>
+                            {
+                                index !== cleanedPathArr.length - 1 ?
+                                    <Link>/ {path}</Link> :
+                                    <span>/ {path}</span>
+                            }
+                        </li>
+                    )
+                })
+            }
+        </ul>
+    )
+}
+
+export default BreadCrump
